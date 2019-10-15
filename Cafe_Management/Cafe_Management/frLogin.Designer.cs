eca4135b512f0dfeefd8c10042d8660eef9f978c
hung156 <17110156@student.hcmute.edu.vn>
2019-10-15 12:09:18 +0700

    Update

--- Cafe_Management/Cafe_Management/frLogin.Designer.cs
+++ Cafe_Management/Cafe_Management/frLogin.Designer.cs
@@ -32,13 +32,17 @@
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.txtUserName = new DevExpress.XtraEditors.TextEdit();
             this.txtPassword = new DevExpress.XtraEditors.TextEdit();
+            this.btn_Login = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_exit = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.txtUserName.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(21, 68);
+            this.labelControl1.Location = new System.Drawing.Point(29, 77);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(102, 17);
             this.labelControl1.TabIndex = 0;
@@ -47,7 +51,7 @@
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(21, 115);
+            this.labelControl2.Location = new System.Drawing.Point(29, 124);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(65, 17);
             this.labelControl2.TabIndex = 1;
@@ -55,24 +59,61 @@
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(143, 65);
+            this.txtUserName.Location = new System.Drawing.Point(151, 74);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(163, 22);
+            this.txtUserName.Size = new System.Drawing.Size(144, 22);
             this.txtUserName.TabIndex = 2;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(143, 110);
+            this.txtPassword.Location = new System.Drawing.Point(151, 119);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(163, 22);
+            this.txtPassword.Size = new System.Drawing.Size(144, 22);
             this.txtPassword.TabIndex = 3;
             this.txtPassword.EditValueChanged += new System.EventHandler(this.txtPassword_EditValueChanged);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(29, 160);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(119, 42);
+            this.btn_Login.TabIndex = 4;
+            this.btn_Login.Text = "Đăng nhập";
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(176, 160);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(119, 42);
+            this.btn_exit.TabIndex = 5;
+            this.btn_exit.Text = "Thoát";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(112, 41);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(0, 16);
+            this.labelControl3.TabIndex = 6;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.labelControl4.Appearance.Options.UseFont = true;
+            this.labelControl4.Location = new System.Drawing.Point(17, 24);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(292, 24);
+            this.labelControl4.TabIndex = 7;
+            this.labelControl4.Text = "PHẦN MỀM QUẢN LÝ QUÁN CAFE";
             // 
             // frLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 159);
+            this.ClientSize = new System.Drawing.Size(321, 223);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_Login);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.labelControl2);
@@ -92,5 +133,9 @@
         private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.TextEdit txtUserName;
         private DevExpress.XtraEditors.TextEdit txtPassword;
+        private DevExpress.XtraEditors.SimpleButton btn_Login;
+        private DevExpress.XtraEditors.SimpleButton btn_exit;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
     }
 }
