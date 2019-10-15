2439d1f1f4baeb98a5b68d715e5a9d164e62c3a2
hung156 <17110156@student.hcmute.edu.vn>
2019-10-15 22:54:12 +0700

    Create frMain, frManage,frLogin

---
+++ Cafe_Management/Cafe_Management/GUI/frMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Cafe_Management.GUI
+{
+    public partial class frMain : DevExpress.XtraEditors.XtraForm
+    {
+        public frMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
